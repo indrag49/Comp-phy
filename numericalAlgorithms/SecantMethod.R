58806690b52c4b146e802f662fa2e70932535c8c
Indranil Ghosh <indranilg49@gmail.com>
2018-01-29 01:30:56 +0530

    Add files via upload

---
+++ numericalAlgorithms/SecantMethod.R
@@ -0,0 +1,20 @@
+## Author: Indranil Ghosh, Jadavpur University, Physics Department
+
+## Secant method
+
+f <- function(x) x*exp(x)-cos(x)
+f_dashed <- function(x) x*exp(x)+exp(x)+sin(x)
+Secant <- function(x0, x1, Max, epsilon) {
+  k <- 1
+  while(k<=Max) {
+    f0 <- f(x0)
+    f1 <- f(x1)
+    x2 <- x1-f1*(x1-x0)/(f1-f0)
+    if(abs(x2 - x1)<epsilon) return (x2)
+    x0 <- x1
+    x1 <- x2
+    k <- k + 1
+  }
+  return ("error: maximum limit reached")
+}
+Secant(0.5, 1, 20, 0.0001)
